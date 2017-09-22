8a0db702a2e063d557af34ab6e84e0c7397b9904
Catislin <caitlin.estrellita@gmail.com>
2017-09-22 09:07:59 -0700

    more flask stuff

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import stochastic
+import histogram
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    normalized_string = (histogram.normalize(histogram.read_in_file('fish.txt')))
+    fish_words = histogram.create_dict(normalized_string)
+    fish_probs = stochastic.probability_dictionary(fish_words)
+    probs_list = stochastic.create_probs_list(fish_probs)
+    return stochastic.random_weighted(probs_list)
