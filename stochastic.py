50d74beb543fd6a7d814ab30e1f970bf05a8afe5
Catislin <caitlin.estrellita@gmail.com>
2017-09-21 20:01:45 -0700

    functional random sampling using probability ranges

--- stochastic.py
+++ stochastic.py
@@ -1,7 +1,8 @@
 import histogram
 import random
-import operator
+from operator import itemgetter
 from datetime import datetime
+from flask import Flask
 
 # takes in a histogram and returns a random word from it
 def random_word(hist):
@@ -34,9 +35,22 @@ def random_weighted_one(hist):
             max_word = word
     return max_word
 
-def random_weighted_two(hist):
-    pass
+def create_probs_list(prob_dict):
+    return_list = []
+    current = 0
+    for item in prob_dict:
+        range_list = []
+        range_list.append(current)
+        current += prob_dict[item]
+        range_list.append(current)
+        return_list.append([item, range_list])
+    return return_list
 
+def random_weighted_two(probs_list):
+    random_probability = random.random() # [0, 1)
+    for pair in probs_list:
+        if random_probability >= pair[1][0] and random_probability < pair[1][1]:
+            return pair[0]
 
 # takes in a histogram dictionary and returns a dictionary in which
 # the key is the word and the value is its probability in the text
@@ -55,32 +69,18 @@ def testing(hist):
     reps = 1000
     start = datetime.now()
     probs = probability_dictionary(hist)
+    print("Probabilities")
     print(probs)
+    probs_list = create_probs_list(probs)
 
-    # testing method two
+    #testing method two
     output = []
     for i in range (reps):
-        output.append(random_weighted_two(hist))
+        output.append(random_weighted_two(probs_list))
     new_hist = histogram.create_dict(" ".join(output))
-    print(new_hist)
+    generated_probs = probability_dictionary(new_hist)
+    print(generated_probs)
 
-    # print the frequency of the word being output by the function
-    # and the frequency of the word in the source text
-    # for word in new_hist.keys():
-    #    print("Word: " + word + "     Frequency output: " + str(new_hist[word] / 1000) + "      Original frequency: " + str(hist[word] / histogram.unique_words(hist)))
-    # print(new_hist)
-
-    # sort new histogram and original histogram by vales and compare
-
-    # print("Original: ")
-    # original_hist = (sorted(hist.items(), key=operator.itemgetter(1), reverse=True))
-    # print("New: ")
-    # words_generated_histogram = (sorted(new_hist.items(), key=operator.itemgetter(1), reverse=True))
-    for word in new_hist.keys():
-        print(word)
-        print("Generated: " + str(new_hist[word] / reps))
-        print("Original:  " + str(probs[word]))
-        print()
     print("Time: " + str(datetime.now() - start))
 
 if __name__ == '__main__':
