faa671652996fb542802ab00d17dc0adc2c18ab0
Catislin <caitlin.estrellita@gmail.com>
2017-09-23 15:01:46 -0700

    added html template

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import stochastic
 import histogram
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
+    '''
     num = request.args.get('num', default = 10, type = int)
     normalized_string = (histogram.normalize(histogram.read_in_file('blue.txt')))
     blue_words = histogram.create_dict(normalized_string)
@@ -13,6 +14,8 @@ def hello():
     for i in range(num):
         return_list.append(stochastic.random_weighted(probs_list))
     return " ".join(return_list)
+    '''
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
