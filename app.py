a452354bc16fae6da5e62e777334aa8c67834434
Catislin <caitlin.estrellita@gmail.com>
2017-09-23 14:44:28 -0700

    adding url parameters

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    num = request.args.get('num', default = 10, type = int)
     normalized_string = (histogram.normalize(histogram.read_in_file('blue.txt')))
     blue_words = histogram.create_dict(normalized_string)
     probs_list = stochastic.create_ranges_list(blue_words)
     return_list = []
-    for i in range(10):
+    for i in range(num):
         return_list.append(stochastic.random_weighted(probs_list))
     return " ".join(return_list)
 
