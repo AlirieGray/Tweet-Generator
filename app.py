34df9703df7736638d302a09f208d81c537d1866
Catislin <caitlin.estrellita@gmail.com>
2017-09-23 13:10:51 -0700

    added stochastic test to app.py

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return("hello world")
-    # normalized_string = (histogram.normalize(histogram.read_in_file('fish.txt')))
-    # fish_words = histogram.create_dict(normalized_string)
-    # fish_probs = stochastic.probability_dictionary(fish_words)
-    # probs_list = stochastic.create_probs_list(fish_probs)
-    # return stochastic.random_weighted(probs_list)
+    normalized_string = (histogram.normalize(histogram.read_in_file('fish.txt')))
+    fish_words = histogram.create_dict(normalized_string)
+    fish_probs = stochastic.probability_dictionary(fish_words)
+    probs_list = stochastic.create_probs_list(fish_probs)
+    return stochastic.random_weighted(probs_list)
+
+if __name__ == "__main__":
+    app.run()
