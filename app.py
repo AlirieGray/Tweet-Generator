f13ff39fe3ae46aaa39e923d562ffb714fa96fa8
Catislin <caitlin.estrellita@gmail.com>
2017-09-26 00:29:36 -0700

    added favorites button

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, render_template
 import stochastic
 import histogram
+import time
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def hello():
     num = request.args.get('num', default = 10, type = int)
     normalized_string = (histogram.normalize(histogram.read_in_file('blue.txt')))
@@ -13,7 +14,15 @@ def hello():
     for i in range(num):
         return_list.append(stochastic.random_weighted(probs_list))
     tweet =  " ".join(return_list)
-    return render_template('index.html', tweet = tweet)
+    if request.method == 'GET':
+        return render_template('index.html', tweet=tweet, time=time.time)
+    elif request.method == 'POST':
+        print("Posting!")
+        return render_template('index.html', tweet=tweet, time=time.time)
+
+@app.route('/favorites', methods=['GET', 'POST'])
+def fav():
+    return render_template('favorites.html', tweet="Placeholder")
 
 if __name__ == "__main__":
     app.run()
