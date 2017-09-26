de86a78e019014d1d4f983fd5b70078dda81fa52
Catislin <caitlin.estrellita@gmail.com>
2017-09-26 12:06:53 -0700

    added store favorites with button feature

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
 import stochastic
 import histogram
 import time
-app = Flask(__name__)
+import json
+app = Flask(__name__, instance_relative_config=True)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class Tweet(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    content = db.Column(db.String(200), unique=True)
+
+    def __init__(self, content):
+        self.content = content
+
+    def __repr__(self):
+        return '<Tweet %r>' % self.content
+
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
@@ -18,11 +33,24 @@ def hello():
         return render_template('index.html', tweet=tweet, time=time.time)
     elif request.method == 'POST':
         print("Posting!")
+        addFavoriteTweet(tweet)
         return render_template('index.html', tweet=tweet, time=time.time)
 
-@app.route('/favorites', methods=['GET', 'POST'])
+@app.route('/favorites', methods=['GET'])
 def fav():
-    return render_template('favorites.html', tweet="Placeholder")
+    # try to get content of tweets in db
+    tweets_list = Tweet.query.all()
+    tweet_strings = []
+    print("Printing all content")
+    for tweet in tweets_list:
+        tweet_strings.append(tweet.content)
+    return render_template('favorites.html', tweet=tweet_strings)
+
+def addFavoriteTweet(tweet_content):
+    tweet = Tweet(tweet_content)
+    db.session.add(tweet)
+    db.session.commit()
+    print(Tweet.query.all())
 
 if __name__ == "__main__":
     app.run()
